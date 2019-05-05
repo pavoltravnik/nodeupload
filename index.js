0bf047352994ae14fe07ef70676e945764ea6ed2
Pavol Travnik <pavol.travnik@protonmail.com>
2019-05-05 15:56:31 +0200

    process

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const multer = require('multer')
 const cors = require('cors');
-var upload = multer({ dest: '../export/' })
+var upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 
 app.get('/upload', function (req, res) {
     res.send('hello world');
 });
-
 
 app.post('/upload', upload.single('file'), function (req, res, next) {
     console.log(req.file);
