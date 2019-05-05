c7e385d05ca64396e184b369af7045d11b3713e0
Pavol Travnik <pavol.travnik@protonmail.com>
2019-05-05 11:26:23 +0200

    edit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
-app.listen(8081, function () {
-  console.log('app listening on port 8081!')
-})
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
