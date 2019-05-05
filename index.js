d2c3499b02dd32c1a582b25b3bc6c330e1e16424
Pavol Travnik <pavol.travnik@protonmail.com>
2019-05-05 16:42:49 +0200

    edit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const multer = require('multer')
 const cors = require('cors');
+const { exec } = require('child_process');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -28,7 +29,8 @@ app.post('/upload',function(req, res) {
             return res.status(500).json(err);
         }
     exec('docker exec ipfs_host ipfs add /export/'+req.file.filename)
-        .then(a => console.log(a));
+        .then(a => console.log(a))
+        .catch(err => console.log(err));
     return res.sendStatus(200);
     });
 });
