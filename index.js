db01dbeecbc66930c36ea71e74870effc10ed3cd
Pavol Travnik <pavol.travnik@protonmail.com>
2019-05-05 16:27:43 +0200

    edit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '../')
+      cb(null, 'uploads/')
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now())
