df0432e916ba1a952c50e44868d2eaa8341dfbcb
Pavol Travnik <pavol.travnik@protonmail.com>
2019-06-16 18:52:03 +0200

    change

--- utils.js
+++
@@ -1,8 +0,0 @@
-
-exports.hex2a = function (hexx) {
-    var hex = hexx.substring(4).toString();
-    var str = '';
-    for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
