abcd74cfc7fb7a9bc57bc8a7ec82d556a79d86f7
Pavol Travnik <pavol.travnik@protonmail.com>
2019-06-16 18:33:17 +0200

    change

--- index.js
+++ index.js
@@ -25,9 +25,10 @@ app.get('/upload', function (req, res) {
 
 app.get('/getaddressTXs', function (req, res) {
     request('https://api.blockcypher.com/v1/ltc/main/addrs/LeNkYGHa9wkZN88acBh6RnMuDo214xh29G/full?after=1621253', function (error, response, body) {
-        console.error('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        if (error) {
+            return res.status(500).json(error);
+        }
+        return res.status(response.statusCode).json(body);
       });
 
 });
