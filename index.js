488caa6bd0142f4b701455dcbef99a258d0e3d41
Pavol Travnik <pavol.travnik@protonmail.com>
2019-06-17 15:05:31 +0200

    change

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/getaddressTXsBB', function (req, res) {
         if (error) {
             return res.status(500).json(error);
         }
-        console.log(body.txids);
+        console.log(body);
         /*
         try {
             body.txids.map(txid => {
